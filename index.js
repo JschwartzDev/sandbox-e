a0c0cd11c6bd750e390a8c645e02c912491d22d5
Eric DiMare <ericjdimare@gmail.com>
2019-10-12 11:51:46 -0400

    hello world git commit

--- index.js
+++ index.js
@@ -6,44 +6,4 @@ let controlPanel = document.getElementById("controlPanel");
 let main = document.getElementById("main");
 let footer = document.getElementById("footer");
 
-let defaultColor = "#96418a";
-
-let elementsList = [header, footer, main, aside, body[0]];
-
-let resetBtn = document.getElementById("colorReset");
-
-let getNewColor = function() {
-  let rand1 = parseInt(Math.random() * 10);
-  let rand2 = parseInt(Math.random() * 10);
-  let rand3 = parseInt(Math.random() * 10);
-
-  let newColor = `#${rand1}${rand2}${rand3}`;
-  return newColor;
-};
-
-buttonsList[0].addEventListener("click", () => {
-  header.style.backgroundColor = getNewColor();
-  body[0].style.backgroundColor = getNewColor();
-});
-
-buttonsList[1].addEventListener("click", () => {
-  aside.style.backgroundColor = getNewColor();
-});
-
-buttonsList[2].addEventListener("click", () => {
-  main.style.backgroundColor = getNewColor();
-});
-
-buttonsList[4].addEventListener("click", () => {
-  footer.style.backgroundColor = getNewColor();
-});
-
-resetBtn.addEventListener("click", () => {
-  elementsList.forEach(el => {
-    colorReset(el);
-  });
-});
-
-let colorReset = function(el) {
-  el.style.backgroundColor = defaultColor;
-};
+let subDivs = main.children;
